157d1ec2ef5d5795081a4d1dfdbe4f904c4365f6
John Sartain <jhnsartain3@gmail.com>
2017-03-11 15:46:39 -0600

    Added ability to rate and get value

--- app/src/main/java/com/johnsandroidstudiotutorials/getvalueratingbartutorial/MainActivity.java
+++ app/src/main/java/com/johnsandroidstudiotutorials/getvalueratingbartutorial/MainActivity.java
@@ -1,13 +1,36 @@
 package com.johnsandroidstudiotutorials.getvalueratingbartutorial;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Float ratingValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RatingBar ratingRatingBar = (RatingBar) findViewById(R.id.rating_rating_bar);
+        Button submitButton = (Button) findViewById(R.id.submit_button);
+        final TextView ratingDisplayTextView = (TextView) findViewById(R.id.rating_display_text_View);
+
+        ratingRatingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                ratingValue = rating;
+            }
+        });
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ratingDisplayTextView.setText(ratingValue.toString());
+            }
+        });
     }
 }
